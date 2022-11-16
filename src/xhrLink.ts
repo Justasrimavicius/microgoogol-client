56faf684ad068038aedd49ace954a4cfa72e0c14
Justinas <justasrimavicius@gmail.com>
2022-11-16 10:24:10 +0200

    forgot to change the origin path

--- src/xhrLink.ts
+++ src/xhrLink.ts
@@ -1,3 +1,3 @@
-// const link  = 'https://microgoogol.herokuapp.com';
-const link = 'http://localhost:8080';
+const link  = 'https://microgoogol.herokuapp.com';
+// const link = 'http://localhost:8080';
 export default link;
